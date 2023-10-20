e63a9481bc9bf06bf10db81a8c9a637e7ea69ca3
Eugeneowner <ownereugene@gmail.com>
2023-10-20 10:43:45 +0300

    axios

---
+++ src/assets/axiosUrl.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+export const instance = axios.create({
+    baseURL: 'https://backend-coral-five.vercel.app'
+});
+export const instanceToken = axios.create({
+    baseURL: 'https://backend-coral-five.vercel.app',
+    validateStatus: () => true
+});
