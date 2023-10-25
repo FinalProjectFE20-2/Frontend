03c930823dc83c5246d5d3a038c3ab22553d2eb7
Olena Oliianenko <felangelena@gmail.com>
2023-10-25 22:49:30 +0200

    feat: add slider

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import styles from './Slider.module.scss';
+import './Slider.scss';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+
+// <Carousel className={styles.Slider} data-testid="Slider">
+
+function Slider() {
+  return (
+    <Carousel pause='false'>
+      <Carousel.Item interval={3000}>
+        <img
+          className="d-block w-100 h-50"
+          src={img1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <img
+          className="d-block w-100 h-50"
+          src={img2}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <img
+          className="d-block w-100 h-50"
+          src={img3}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+Slider.propTypes = {};
+
+Slider.defaultProps = {};
+
+export default Slider;
---
+++ src/containers/Discounts/Discounts.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import styles from './Discounts.module.scss';
+import Slider from '../../components/Slider/Slider.jsx';
+
+const Discounts = () => (
+  <div className={`${styles.Discounts} container`} data-testid="Discounts">
+    <h2 className={styles.title} >Акційні пропозиції</h2>
+    <Slider />
+  </div>
+);
+
+Discounts.propTypes = {};
+
+Discounts.defaultProps = {};
+
+export default Discounts;
