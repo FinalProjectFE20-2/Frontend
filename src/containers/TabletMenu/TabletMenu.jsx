b551fb7ebbb1f65bf5735751381b907e232db1a3
Наталія Коломієць <kolomiiets.nataliia0905@gmail.com>
2023-10-27 18:14:05 +0300

    hot fix

--- src/containers/TabletMenu/TabletMenu.jsx
+++ src/containers/TabletMenu/TabletMenu.jsx
@@ -1,10 +1,12 @@
 import styles from './TabletMenu.module.css';
 import Action from './Action/Action.jsx';
+import ProductsCatigories from './ProductsCatigories/ProductsCatigories';
 
 export default function TabletMenu() {
   return (
     <div className={styles.wrapper}>
-      <Action upPrice={30} downprice={20} />
+      {/* <Action upPrice={30} downprice={20} /> */}
+      <ProductsCatigories />
     </div>
   );
 }
