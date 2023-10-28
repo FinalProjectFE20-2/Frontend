bdb8e2a9fabd61c7591265d211a4af184cfdc50f
Olena Oliianenko <felangelena@gmail.com>
2023-10-28 19:12:52 +0200

    feat: add slider media queries

--- src/containers/Discounts/Discounts.jsx
+++ src/containers/Discounts/Discounts.jsx
@@ -3,7 +3,7 @@ import styles from './Discounts.module.scss';
 import Slider from '../../components/Slider/Slider.jsx';
 
 const Discounts = () => (
-  <div className={`${styles.Discounts} container`} data-testid="Discounts">
+  <div className={`${styles.Discounts} ${styles.container}`} data-testid="Discounts">
     <h2 className={styles.title} >Акційні пропозиції</h2>
     <Slider />
   </div>
