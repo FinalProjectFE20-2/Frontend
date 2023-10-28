d5a52042bd314db88ab7a790c1037e00ba63123c
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2023-10-28 20:26:39 +0300

    merge main

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../App/App';
+import App from '@/App.jsx';
 
 // eslint-disable-next-line import/prefer-default-export
 export const router = createBrowserRouter([
