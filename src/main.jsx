731c28229ac8d7334cbaf8b925b6c59dfbc6f7fa
Olena Oliianenko <felangelena@gmail.com>
2023-10-29 16:59:46 +0100

    feat: changed z-index for Discounts delete unnesasery css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './App.module.css';
 import { router } from './routers/routers.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
