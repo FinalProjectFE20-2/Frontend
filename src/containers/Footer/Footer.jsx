6293c1f0a58c56c6f28892f0be9505085f0139dd
Olena Oliianenko <felangelena@gmail.com>
2023-10-29 19:46:09 +0100

    feat: add media queries to Footer

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import HeaderLogo from '../Header/components/HeaderLogo/HeaderLogo';
 
 const Footer = () => (
   <section className={styles.Footer} data-testid="Footer">
-    Footer Component
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.innerWrapper}>
+          <HeaderLogo />
+          <div className={styles.links}>
+            <a href='#' className={styles.link}>Зворотній зв'язок</a>
+            <a href='#' className={styles.link}>Доставка</a>
+            <a href='#' className={styles.link}>Оплата</a>
+            <a href='#' className={styles.link}>Контакти</a>
+          </div>
+        </div>
+        <div className={styles.contacts}>
+          <a href="tel:+380938416729" className={`${styles.phone} ${styles.link}`}>+38 (093) 841-67-29</a>
+          <a href="mailto: delivery@midas.rest" className={`${styles.email} ${styles.link}`}>delivery@midas.rest</a>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.wrapperPolitics}>
+        <p className={styles.rights}>©2023 «MIDAS», DAN IT</p>
+        <div className={styles.politics}>
+          <a href='#' className={styles.link}>Політика конфіденційності і оферта</a>
+          <a href='#' className={styles.link}>Угода використання</a>
+        </div>
+      </div>
+
+    </div>
   </section>
 );
-
-Footer.propTypes = {};
-
-Footer.defaultProps = {};
 
 export default Footer;
