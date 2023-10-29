5ad14af04bb4c754b9287b443c7a4952883c17fb
Наталія Коломієць <kolomiiets.nataliia0905@gmail.com>
2023-10-29 10:45:29 +0200

    add

--- src/containers/TabletMenu/ProductsCatigories/ProductsCatigories.jsx
+++ src/containers/TabletMenu/ProductsCatigories/ProductsCatigories.jsx
@@ -4,39 +4,19 @@ import ProdCategoriesItem from './ProdCategoriesItem/ProdCategoriesItem.jsx';
 import styles from './ProductsCatigories.module.css';
 
 export default function ProductsCatigories() {
-  // const data = [
-  //   {
-  //     title: 'Гаряче',
-  //     items: [
-  //       {
-  //         title: 'Гарячі страви',
-  //         img: '@/assets/products/17.png',
-  //       },
-  //
-  //       {
-  //         title: 'Супи',
-  //         img: '@/assets/products/17.png',
-  //       },
-  //       {
-  //         title: 'Хінкалі',
-  //         img: '@/assets/products/17.png',
-  //       },
-  //     ],
-  //   },
-  // ];
-
-  const withoutSubmenu = [];
-  const withSubmenu = [];
-  let action;
-  menuItems.forEach((item) => {
-    if (item.submenu) {
-      withSubmenu.push(item);
-    } else if (item.title.toLowerCase() !== 'акції') {
-      withoutSubmenu.push(item);
-    } else {
-      action = item;
-    }
-  });
+  const { withSubmenu, withoutSubmenu, action } = menuItems.reduce(
+    (acc, item) => {
+      if (item.submenu) {
+        acc.withSubmenu.push(item);
+      } else if (item.title.toLowerCase() !== 'акції') {
+        acc.withoutSubmenu.push(item);
+      } else {
+        acc.action = item;
+      }
+      return acc;
+    },
+    { withSubmenu: [], withoutSubmenu: [], action: null },
+  );
 
   return (
     <div className={styles.categories}>
@@ -44,49 +24,24 @@ export default function ProductsCatigories() {
         <Action upPrice={30} downprice={20} obj={action} />{' '}
       </div>
       <ul className={styles.list}>
-        {withSubmenu.map((item) => (
+        {withSubmenu.map(item => (
           <li className={styles.listItem}>
             <h2 className={styles.title}>{item.title}</h2>
             <ul className={styles.submenuList}>
-              {item.submenu.map((subItem) => (
+              {item.submenu.map(subItem => (
                 <ProdCategoriesItem item={subItem} />
               ))}
             </ul>
           </li>
         ))}
         <li className={`${styles.listItem} ${styles.withoutSubmenu}`}>
-          {withoutSubmenu.map((item) => (
-            <ProdCategoriesItem item={item} />
-          ))}
+          <ul>
+            {withoutSubmenu.map(item => (
+              <ProdCategoriesItem item={item} />
+            ))}
+          </ul>
         </li>
       </ul>
     </div>
   );
 }
-
-// return (
-//   <ul className={styles.categories}>
-//     {menuItems.map((item) => {
-//       if (item.title.toLowerCase() === "акції") {
-//         return (
-//           <li>
-//             <Action upPrice={30} downprice={20} obj={action} />{" "}
-//           </li>
-//         );
-//       }
-//       if (item.submenu?.length) {
-//         return (
-//           <li>
-//             <h2>{item.title}</h2>
-//             <ul>
-//               {item.submenu.map((subItem) => (
-//                 <ProdCategoriesItem item={subItem} />
-//               ))}
-//             </ul>
-//           </li>
-//         );
-//       }
-//       return <ProdCategoriesItem item={item} />;
-//     })}
-//   </ul>
-// );
