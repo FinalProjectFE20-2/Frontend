771ca1cce4f036375ec6e6408b17c68aad8d50e2
Наталія Коломієць <kolomiiets.nataliia0905@gmail.com>
2023-10-29 14:20:00 +0200

    add merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import PopularDishesMenu from './components/PopularDishesMenu';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+
+import "./index.css"
 import Discounts from './containers/Discounts/Discounts.jsx';
 import Header from '@/containers/Header/Header.jsx';
 
 function App() {
   return (
     <>
+
       <Header />
       <PopularDishesMenu />
       <Discounts />
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
+import './App.module.css';
 import { router } from './routers/routers.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
