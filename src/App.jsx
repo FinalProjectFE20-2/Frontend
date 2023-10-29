d88e9614060ead81f31fd9b8eb3703aa63c5cbce
Olena Oliianenko <felangelena@gmail.com>
2023-10-29 16:59:46 +0100

    feat: changed z-index for Discounts delete unnesasery css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import PopularDishesMenu from './components/PopularDishesMenu';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import "./index.css"
-import Discounts from './containers/Discounts/Discounts.jsx';
+import "./styles/index.scss"
 import Header from '@/containers/Header/Header.jsx';
+import PopularDishesMenu from './components/PopularDishesMenu';
+import Discounts from './containers/Discounts/Discounts.jsx';
 
 function App() {
   return (
     <>
-
       <Header />
       <PopularDishesMenu />
       <Discounts />
