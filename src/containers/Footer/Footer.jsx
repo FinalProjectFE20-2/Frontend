77d30f473e85ccf6fd9ded9baa81e6e79636663f
Olena Oliianenko <felangelena@gmail.com>
2023-10-29 17:03:35 +0100

    feat: add Footer

---
+++ src/containers/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+const Footer = () => (
+  <section className={styles.Footer} data-testid="Footer">
+    Footer Component
+  </section>
+);
+
+Footer.propTypes = {};
+
+Footer.defaultProps = {};
+
+export default Footer;
