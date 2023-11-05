f4c92dbadce5631d787aec59e7be05fd0ab03aec
user <user@MacBook-Pro-4.local>
2023-11-05 14:25:45 +0200

    routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/index.scss"
-import Header from '@/containers/Header/Header.jsx';
 import PopularDishesMenu from './components/PopularDishesMenu';
 import Discounts from './containers/Discounts/Discounts.jsx';
 import Footer from './containers/Footer/Footer.jsx';
@@ -8,7 +7,6 @@ import Footer from './containers/Footer/Footer.jsx';
 function App() {
   return (
     <>
-      <Header />
       <PopularDishesMenu />
       <Discounts />
       <Footer />
--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,11 +1,60 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 import App from '@/App.jsx';
+import Header from '@/containers/Header/Header.jsx';
 
 // eslint-disable-next-line import/prefer-default-export
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
+export const router = createBrowserRouter([{
+    element:
+      <>
+        <Header />
+        <Outlet />
+      </>,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        element: <div>Акції</div>,
+        path: '/action',
+      },
+      {
+        element: <div>Гарячі страви</div>,
+        path: '/hot_dishes',
+      },
+      {
+        element: <div>Супи</div>,
+        path: '/soups',
+      },
+      {
+        element: <div>Хінкалі</div>,
+        path: '/khinkali',
+      },
+      {
+        element: <div>Холодні страви</div>,
+        path: '/cold_dishes',
+      },
+      {
+        element: <div>Салати</div>,
+        path: '/salat',
+      },
+      {
+        element: <div>Соуси</div>,
+        path: '/souse',
+      },
+      {
+        element: <div>Свіжа випічка</div>,
+        path: '/fresh_pastries',
+      },
+      {
+        element: <div>Десерти</div>,
+        path: '/deserts',
+      },
+      {
+        element: <div>Напої</div>,
+        path: '/drinks',
+      },
+    ],
+  }],
+);
