b54e694d4c9503b19df4893e29253a9f51ca5b54
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-11-05 14:19:13 +0200

    main commit 1.0

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,11 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App.jsx';
+import Checkout from '@/containers/Checkout/Checkout.jsx';
+
 
 // eslint-disable-next-line import/prefer-default-export
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/checkout', 
+    element: <Checkout />, 
   },
 ]);
