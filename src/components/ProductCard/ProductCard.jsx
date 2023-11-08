9412ef6eaaadab37b6841d157f9d9731ceea1373
Olena Oliianenko <felangelena@gmail.com>
2023-11-08 19:52:52 +0100

    feat: add styles to ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import Basket from '@/assets/svg_icon/navigation/Basket.svg?react';
+import {  Link } from "react-router-dom";
+import styles from './ProductCard.module.scss';
+
 const ProductCard = ({ itemNo }) => {
   // const product1 = {
   //   _id: 1,
@@ -19,19 +22,22 @@ const ProductCard = ({ itemNo }) => {
   //   quantity: 100,
   // };
   const products = useSelector(state => state.products.products || []);
-const product = products.find(product => product.itemNo===itemNo)
+  const product = products.find(product => product.itemNo===itemNo);
   console.log(product, 11);
 
   return (
-    <li>
+    <li className={styles.card}>
       {product.imageUrls?.length > 0 && (
-        <img src={product?.imageUrls[0]} alt={product?.name} />
+        <img className={styles.img} src={product?.imageUrls[0]} alt={product?.name} />
       )}
-      <h4>{product?.name}</h4>
-      <p>{product.sizes}</p>
-      <p>{product.manufacturer}</p>
-      <p>&#8372; {product.previousPrice},00</p>
-      {!!product.currentPrice && <p>&#8372; {product.currentPrice},00</p>}
+      <h4 className={styles.title}>{product?.name}</h4>
+      <p className={styles.weight}>{product.sizes}</p>
+      <p className={styles.info}>{product.manufacturer}</p>
+      <div className={styles.footer}>
+        <p className={styles.price}>&#8372; {product.previousPrice},00</p>
+        {!!product.currentPrice && <p>&#8372; {product.currentPrice},00</p>}
+        <Link to={`/product/${product.id}`} ><Basket className='svg'/></Link>
+      </div>
     </li>
   );
 };
