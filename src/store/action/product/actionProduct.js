3f383ec1dbefa2636dc8eaa3de895d545fa54eff
Eugeneowner <ownereugene@gmail.com>
2023-11-10 18:10:01 +0200

    file

--- src/store/action/product/actionProduct.js
+++ src/store/action/product/actionProduct.js
@@ -5,7 +5,9 @@ const getProducts = data => ({
 });
 
 export const getAllProducts = () => async dispatch => {
-  const res = await fetch('https://backend-zeta-sandy.vercel.app/api/products');
+  const res = await fetch(
+    `https://backend-zeta-sandy.vercel.app/api/products?perPage=7&startPage=2`,
+  );
   // const res = await fetch('http://localhost:4000/api/products');
   const products = await res.json();
   console.log(products, 333);
