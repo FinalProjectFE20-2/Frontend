1bf1b02fe8ace2e797ef7185502fd55ab3bf1985
Olena Oliianenko <felangelena@gmail.com>
2023-11-10 20:39:22 +0100

    feat: add links to products, add data links to ProductContainer

--- src/containers/ProductContainer/ProductContainer.jsx
+++ src/containers/ProductContainer/ProductContainer.jsx
@@ -5,26 +5,26 @@ import { useSelector } from 'react-redux';
 
 const ProductContainer = ({productId}) => {
   const products = useSelector(state => state.products.products || []);
-  const product = products.find(product => product.productId===productId);
+  const product = products.find(product => product._id===productId);
   console.log(product);
 
   return(
     <section className={`${styles.productContainer} container`}>
-      <h2 className={styles.title}>Хінкали смажені ${productId}</h2>
+      <h2 className={styles.title}>{product.name}</h2>
       <h3 className={styles.route_road}> Home / <span className={styles.route_active}>Product</span></h3>
       <div className={styles.grid}>
         <div>
-          <img className={styles.img} src="./img/img1.jpg" alt="dish 1" />
+          <img className={styles.img} src={product?.imageUrls[0]} alt={product?.name} />
         </div>
         <div className={styles.desc__box}>
-          <p className={styles.mgb}><span className={styles.desc__title}>Вага: </span><span className={styles.desc__info}>250 г</span></p>
+          <p className={styles.mgb}><span className={styles.desc__title}>Вага: </span><span className={styles.desc__info}>{product.sizes}</span></p>
 
           <div className={styles.mgb}>
             <p className={styles.desc__title}>Опис:</p>
-            <p className={styles.desc__info}>Плотно-шелковые хинкали с сочной начинкой на выбор: из фермерской говядины, сыра моцарелла либо фермерской баранины и экологически чистой зелени</p>
+            <p className={styles.desc__info}>{product.manufacturer}</p>
           </div>
 
-          <p className={styles.mgb}><span className={styles.desc__title}>Ціна: </span><span className={styles.desc__info}>250 грн.</span></p>
+          <p className={styles.mgb}><span className={styles.desc__title}>Ціна: </span><span className={styles.desc__info}> {!product.currentPrice && product.previousPrice},00 грн.</span></p>
 
           <div className={styles.desc__counter}>
             <div className={styles.counter__group}>
@@ -32,7 +32,7 @@ const ProductContainer = ({productId}) => {
               <input className={styles.counter__input} type='number' placeholder='1'/>
               <button className={`${styles.counter__btn} ${styles.counter__btn} ${styles.btn__plus}`}>+</button>
             </div>
-            <p className={styles.desc__price}>₴ 250,00</p>
+            <p className={styles.desc__price}>₴ {!product.currentPrice && product.previousPrice},00</p>
             <button><Basket className='svg'/></button>
           </div>
         </div>
