2e15846cacba1a494ea9339b06fca33dc5d5760a
Olena Oliianenko <felangelena@gmail.com>
2023-11-14 22:12:07 +0100

    feat: counter in OrderBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./styles/index.scss";
 import Hero from './containers/Hero/Hero.jsx';
 import PopularDishesMenu from './containers/PopularDishesMenu/PopularDishesMenu.jsx';
 import Discounts from './containers/Discounts/Discounts.jsx';
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getAllProducts } from './store/action/product/actionProduct';
 import { useEffect } from 'react';
 
@@ -15,12 +15,6 @@ function App() {
     const startPage = Math.floor(Math.random() * 5) + 1;
     dispatch(getAllProducts(perPage, startPage));
   }, [dispatch]);
-
-  const cart = useSelector((state) => state.cart.cart);
-  // Save cart in Local Storage on every change
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  },[cart]);
 
   return (
     <>
--- src/store/action/cartAction.js
+++
@@ -1,7 +0,0 @@
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const DELETE_FROM_CART = 'DELETE_FROM_CART';
-export const CLEAR_CART = 'CLEAR_CART';
-
-export const addToCartAC = (id, amount=1) => ({type: ADD_TO_CART, payload: {id, amount}});
-export const deleteFromCartAC = (id) => ({type: DELETE_FROM_CART, payload: id});
-export const clearCartAC = () => ({type: CLEAR_CART});
