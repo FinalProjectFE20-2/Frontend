0d4b16a56b13f2f8bcbd6262313014eee1f83ebe
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-11-16 21:06:29 +0200

    kosss commit 5.1

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,13 +1,62 @@
-import React, { useState } from 'react';
-import './Contacts.scss';
+import React, { useEffect } from 'react';
+import { LoadScript } from '@react-google-maps/api';
+import styles from './Contacts.module.scss';
 
 const Contacts = () => {
+  const containerStyle = {
+    width: '600px',
+    height: '400px',
+  };
+
+  useEffect(() => {
+    const initMap = () => {
+      const map = new window.google.maps.Map(document.getElementById('map'), {
+        center: { lat: 50.489557, lng: 30.490599 },
+        zoom: 10,
+      });
+
+      new window.google.maps.Marker({
+        position: { lat: 50.489557, lng: 30.490599 },
+        map: map,
+        title: 'Київ, проспект Степана Бандери, буд 125',
+      });
+    };
+
+    if (window.google && window.google.maps) {
+      initMap();
+    } else {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCfqu9rwI_23RFnVD66Lq7UBNJQP98rBkg&libraries=places`;
+      script.async = true;
+      script.defer = true;
+      script.onload = initMap;
+
+      document.head.appendChild(script);
+    }
+  }, []);
+
   return (
-    <div className="container">
-      <div className="container__checkout_kosss1">
-        <button className="confirm-button__checkout_kosss1">Замовити</button>
+    <div className={styles.container}>
+      <div className={styles.containerContactsKosss}>
+        <div className={styles.titleContactsKosss}>
+          <h1>Контакти</h1>
+          <p>Головна / <span style={{ color: '#9EA2AA' }}>Контакти</span></p>
+        </div>
+
+        <div className={styles.detailesContactsKosss}>
+          <div className={styles.mapContactsKosss} id="map"></div>
+          <div className={styles.descriptionContactsKosss}>
+            <h2>Ми знаходимся:</h2>
+            <p>Київ, проспект Степана Бандери, буд 125</p>
+            <h2>Режим роботи:</h2>
+            <p>11:00 - 23:00 </p>
+            <h2>Номер телефону:</h2>
+            <p>+38 (093) 841-67-29</p>
+            <h2>Email:</h2>
+            <p>delivery@midas.rest</p>
+          </div>
+        </div>
       </div>
-      <h1>тут будут КОНТАКТИ</h1>
     </div>
   );
 };
