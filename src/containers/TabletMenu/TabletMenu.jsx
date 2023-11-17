872082d648008fe70fff580e65d13ef0a5656d51
Наталія Коломієць <kolomiiets.nataliia0905@gmail.com>
2023-11-17 14:07:43 +0200

    Nataliia header and burger menu

--- src/containers/TabletMenu/TabletMenu.jsx
+++ src/containers/TabletMenu/TabletMenu.jsx
@@ -1,12 +1,12 @@
 import styles from './TabletMenu.module.scss';
-import Action from './Action/Action.jsx';
+
 import ProductsCatigories from './ProductsCatigories/ProductsCatigories';
 import HeaderContainer from '../../components/HeaderContainer/HeaderContainer.jsx';
 
 export default function TabletMenu() {
   return (
     <div className={styles.wrapper}>
-      <HeaderContainer>
+      <HeaderContainer classAdd={'wrapper'}>
         <ProductsCatigories />
       </HeaderContainer>
     </div>
