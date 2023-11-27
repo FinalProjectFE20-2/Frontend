d32c048e4272d3cff1a525f1c6960afec2600a3b
Yarsvit <149312717+Yarsvit@users.noreply.github.com>
2023-11-27 22:15:07 +0200

    Add files via upload

---
+++ Thank.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Дякуємо за замовлення, наш менеджер зв’яжеться з вами</h1>
+      <h3>наш менеджер зв’яжеться з вами</h3>
+    </div>
+  );
+}
+
+export default App;
+
