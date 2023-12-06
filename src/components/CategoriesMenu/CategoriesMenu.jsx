14debac220249f0e37137cadb978e87b968e6d83
Наталія Коломієць <kolomiiets.nataliia0905@gmail.com>
2023-12-06 12:28:55 +0200

    Nataliia fix scroll

--- src/components/CategoriesMenu/CategoriesMenu.jsx
+++ src/components/CategoriesMenu/CategoriesMenu.jsx
@@ -1,5 +1,6 @@
 import styles from './CategoriesMenu.module.scss';
 import {useMediaQuery} from 'react-responsive';
+import {useEffect} from 'react';
 import Burger from '@/assets/icons/Burger.svg?react';
 import Close from '@/assets/icons/Close.svg?react';
 import Categories from '../Categories/Categories.jsx';
@@ -21,7 +22,18 @@ export default function CategoriesMenu() {
         dispatch(setIsOpen())
     };
     const menu = isTablet ? <TabletMenu/> : <Categories/>;
-    const icon =!isOpen ? <Burger onClick={handleClick}/> : <Close onClick={handleClick}/>;
+    const icon = !isOpen ? <Burger onClick={handleClick}/> : <Close onClick={handleClick}/>;
+    useEffect(() => {
+        const root = document.getElementById('root')
+       let cssValue = isDeckstop && 'height:auto; overflow: scroll;'
+        if (typeof cssValue ==='boolean') {
+            cssValue = isOpen ? 'height:100vh; overflow: hidden;': 'height:auto; overflow: scroll;'
+        }
+
+        root.style.cssText = cssValue;
+
+    }, [isOpen,isDeckstop])
+
     return (
         <>
             <div className={styles.wrapper}>
